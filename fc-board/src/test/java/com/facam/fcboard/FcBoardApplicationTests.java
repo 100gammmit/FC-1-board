b7ebebd68538f3583997e86a08af69340d2f40da
100gammmit <100minha@naver.com>
2022-10-15 19:18:19 +0900

    # spring boot 프로젝트 시작
    초기 dependency:
    * devtools
    * lombok
    * spring web
    * actuator

---
+++ fc-board/src/test/java/com/facam/fcboard/FcBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facam.fcboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
